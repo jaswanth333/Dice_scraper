df540a0482113444f8b3d1298ad21085572c86f5
jaswanth333 <kjaswanth4@gmail.com>
2023-11-08 13:24:13 -0500

    updated parameters

--- sraper.py
+++ sraper.py
@@ -21,7 +21,7 @@ params = {
     'fj': 'true',
     'includeRemote': 'true'}
 
-headers = headers = {
+headers = {
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/112.0',
     'Accept': 'application/json, text/plain, */*',
     'Accept-Language': 'en-US,en;q=0.5',
