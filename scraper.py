854c824432a0df45fca810933b74e1d352137ccb
jaswanth333 <kjaswanth4@gmail.com>
2023-11-15 12:23:48 -0500

    updated scraper.py

--- scraper.py
+++ scraper.py
@@ -5,21 +5,24 @@ from bs4 import BeautifulSoup
 
 url = "https://job-search-api.svc.dhigroupinc.com/v1/dice/jobs/search"
 
-params = {
-    'q': 'data engineer',
-    'locationPrecision': 'Country',
-    'countryCode2': 'US',
-    'page': 1,
-    'pageSize': 100,
-    'facets': 'employmentType|postedDate|workFromHomeAvailability|employerType|easyApply|isRemote',
-    'filters.employmentType': 'CONTRACTS',
-    'filters.postedDate': 'ONE',
-    'fields': 'id|jobId|guid|summary|title|postedDate|modifiedDate|jobLocation.displayName|detailsPageUrl|salary|clientBrandId|companyPageUrl|companyLogoUrl|positionId|companyName|employmentType|isHighlighted|score|easyApply|employerType|workFromHomeAvailability|isRemote|debug',
-    'culture': 'en',
-    'recommendations': 'true',
-    'interactionId': 0,
-    'fj': 'true',
-    'includeRemote': 'true'}
+params = {'q': 'data engineer',
+          'locationPrecision': 'Country',
+          'latitude': '37.09024',
+          'longitude': '-95.712891',
+          'countryCode2': 'US',
+          'radius': '30',
+          'radiusUnit': 'mi',
+          'page': 1,
+          'pageSize': 100,
+          'facets': 'employmentType|postedDate|workFromHomeAvailability|employerType|easyApply|isRemote',
+          'filters.employmentType': 'CONTRACTS',
+          'filters.postedDate': 'ONE',
+          'fields': 'id|jobId|guid|summary|title|postedDate|modifiedDate|jobLocation.displayName|detailsPageUrl|salary|clientBrandId|companyPageUrl|companyLogoUrl|positionId|companyName|employmentType|isHighlighted|score|easyApply|employerType|workFromHomeAvailability|isRemote|debug',
+          'culture': 'en',
+          'recommendations': 'true',
+          'interactionId': '0',
+          'fj': 'true',
+          'includeRemote': 'true'}
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/112.0',
@@ -38,40 +41,39 @@ headers = {
 links = []
 results = []
 
-for pages in range(0, int(params['page'])):
-    response = requests.request("GET", url, headers=headers, params=params)
-    json_data = response.json()
-    for jobs in json_data['data']:
-        links.append(jobs['detailsPageUrl'])
+response = requests.request("GET", url, headers=headers, params=params)
+json_data = response.json()
+for jobs in json_data['data']:
+    links.append(jobs['detailsPageUrl'])
 
-        values = {
-            'title': jobs['title'],
-            'job_url': jobs['detailsPageUrl'],
-            'city': jobs['jobLocation']['displayName'].split(',')[0],
-            'state': jobs['jobLocation']['displayName'].split(',')[1],
-            'modified_date': datetime.strptime(jobs['modifiedDate'], "%Y-%m-%dT%H:%M:%SZ").date(),
-            'posted_date': datetime.strptime(jobs['postedDate'], "%Y-%m-%dT%H:%M:%SZ").date(),
-            'company_name': jobs['companyName']
-        }
+    values = {
+        'title': jobs['title'],
+        'job_url': jobs['detailsPageUrl'],
+        'city': jobs['jobLocation']['displayName'].split(',')[0],
+        'state': jobs['jobLocation']['displayName'].split(',')[1],
+        'modified_date': datetime.strptime(jobs['modifiedDate'], "%Y-%m-%dT%H:%M:%SZ").date(),
+        'posted_date': datetime.strptime(jobs['postedDate'], "%Y-%m-%dT%H:%M:%SZ").date(),
+        'company_name': jobs['companyName']
+    }
 
-        r = requests.get(jobs['detailsPageUrl'], headers=headers)
-        soup = BeautifulSoup(r.content, 'lxml')
-        values['time_ago'] = soup.find('span', id='timeAgo').text.split('|')[0]
+    r = requests.get(jobs['detailsPageUrl'], headers=headers)
+    soup = BeautifulSoup(r.content, 'lxml')
+    values['time_ago'] = soup.find('span', id='timeAgo').text.split('|')[0]
 
-        contract_type = []
-        temp = soup.find('div', attrs={'data-cy': 'employmentDetails'})
-        for e in temp.find_all('span', class_="chip_chip__cYJs6"):
-            if e:
-                contract_type.append(e.text)
-                job_types = ','.join(contract_type)
-            else:
-                job_types = contract_type.append('None')
-
-        if 'Accepts corp to corp applications' in job_types:
-            values['contract_type'] = 'C2C'
-            results.append(values)
+    contract_type = []
+    temp = soup.find('div', attrs={'data-cy': 'employmentDetails'})
+    for e in temp.find_all('span', class_="chip_chip__cYJs6"):
+        if e:
+            contract_type.append(e.text)
+            job_types = ','.join(contract_type)
         else:
-            pass
+            job_types = contract_type.append('None')
+
+    if 'Accepts corp to corp applications' in job_types:
+        values['contract_type'] = 'C2C'
+        results.append(values)
+    else:
+        continue
 
 df = pd.DataFrame(results)
-df.to_csv('results.csv', index=False)
+df.to_csv('C2C_results.csv', index=False)
